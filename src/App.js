b3b5d571f851d4bc120e46be3dbf31b7d31476c3
My20123 <nguyentramy02082016@gmail.com>
2023-02-05 11:11:26 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero";
+import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+     <Hero/>
+     <Analytics/>
+     <Newsletter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Analytics.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Analytics = () => {
+    return (
+        <div className="w-full bg-white py-16 px-4">
+            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+                <img src="djjf" alt="/image" />
+                <div className="flex flex-col justify-center"> 
+                    <p className="text-[#00df9a] font-bold">DATA ANALYTICS DASHBOARD</p>
+                    <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Manage Data Analytics Centrally</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat laudantium dignissimos itaque libero magni laboriosam recusandae nam error placeat, hic, nisi, eos atque debitis aut distinctio maiores consequuntur ratione a.</p>
+                    <button className="text-[#00df9a] bg-black w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 ">Get Started</button>
+                </div>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Analytics
